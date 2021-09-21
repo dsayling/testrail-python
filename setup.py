f423372f2d82275d8134f675e9bdd4018399aa31
Drew Ayling <drew.ayling@livongo.com>
2021-09-21 15:27:13 -0600

    fetch all

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup
 
 setup(
-    use_scm_version=dict(tag_regex=r"^(?P<prefix>example-)?(?P<version>[vV]?\d+(?:\.\d+){0,2}[^\+]*)(?:\+.*)?$")
+    use_scm_version={'tag_regex': r"^(?P<prefix>example-)?(?P<version>[vV]?\d+(?:\.\d+){0,2}[^\+]*)(?:\+.*)?$"}
     )
