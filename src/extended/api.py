d9592e0dc95612cdbee4bdcbbb208d070ca59850
Drew Ayling <dsayling@gmail.com>
2021-09-21 15:45:32 -0600

    Add modeled api 2 (#7)
    * starting a modeled, class based approach
    
    * Create __init__.py
    
    * add api call for tcs and fix base class
    
    * more changes
    
    * lint the code now
    
    * cleanup dms, might refactor to separate models
    
    * Update tox.ini
    
    * Update tox.ini

--- src/extended/api.py
+++ src/extended/api.py
@@ -1,7 +1,9 @@
 # todo change to module imports
+from typing import Union
 import time
 from dataclasses import asdict, dataclass, fields
 from typing import Any, List, Union
+
 
 from extended.models import Case, Section, Suite
 from testrail import APIClient
@@ -14,11 +16,9 @@ class TestRail(APIClient):
     def __init__(self, user, password, url):
         super().__init__(user, password, url)
 
-    def get_case(self, case_id):
-        response = self.send_get(f"get_case/{case_id}")
-        field_names = set(f.name for f in fields(Case))
-        if response:
-            return Case(**{k:v for k,v in response.items() if k in field_names})
+    def get_case(self, case_id, model=Case):
+        return model.from_get(self.send_get(f"get_case/{case_id}"))
+
 
     def get_case_types(self):
         return self.send_get("get_case_types")
@@ -29,67 +29,76 @@ class TestRail(APIClient):
     def get_case_fields(self):
         return self.send_get("get_case_fields")
 
-    def get_cases(self, project_id, suite_id):
+    def get_cases(self, project_id, suite_id, model=Case):
+
         if suite_id:
             cases = self.send_get(f"/get_cases/{project_id}&suite_id={suite_id}")
         else:
             cases = self.send_get(f"/get_cases/{project_id}")
-        return [Case(**_) for _ in cases]
+        return [model.from_get(_) for _ in cases]
+
 
     def get_suite(self, suite_id):
         suite = Suite(**self.send_get(f"get_suite/{suite_id}"))
         sections = self.get_sections(suite.project_id, suite.id)
         return (
             suite,
-            sections[0],
-        )  # hopefully we can just have one setion for each suite for now, what is gonna be the best way to
-        # eventually it would be nice to be able to setup
+            sections,
+        )
+
 
     def get_suites(self, project_id):
         suites = [Suite(**_) for _ in self.send_get(f"get_suites/{project_id}")]
         return suites
 
-    def add_suite(self, name: str, description: str, project_id: int):
-        suite = Suite(
-            **self.send_post(
+    def add_suite(
+        self,
+        name: str,
+        description: str,
+        project_id: int,
+        model=Suite,
+        create_default_section=True,
+    ):
+        suite = model.from_get(
+            self.send_post(
                 f"/add_suite/{project_id}", {"name": name, "description": description}
             )
         )
+        section = None
         # next create the default section and then return the suite information
-        section = self.add_section(suite.project_id, Section(suite.id))
-        return suite, section
-
-    def add_case(self, section_id: int, case: Case):
-        response = self.send_post(f"/add_case/{section_id}", asdict(case))
-        field_names = set(f.name for f in fields(Case))
-        if response:
-            return Case(**{k:v for k,v in response.items() if k in field_names})
-
-    def add_section(self, project_id: int, section: Section):
         # new suites are created without cases or a section
         # in the UI, when you add a test case, it creates a default section called, "Test Cases"
         # so, if suites already have a section, get the sections in the suite, if the names match, just use that, otherwise, create a new one
-        return Section(**self.send_post(f"/add_section/{project_id}", asdict(section)))
+        if create_default_section:
+            section = self.add_section(suite.project_id, Section(suite.id))
+        return suite, section
 
-    def get_section(self, section_id: int):
-        return Section(**self.send_get(f"/get_section/{section_id}"))
+    def add_case(self, section_id: int, case: Case):
+        return case.from_get(
+            self.send_post(f"/add_case/{section_id}", case.to_post(case))
+        )
 
-    def get_sections(self, project_id: int, suite_id: Union[int, None] = None):
+    def add_section(self, project_id: int, section: Section):
+        return section.from_get(
+            self.send_post(f"/add_section/{project_id}", section.to_post())
+        )
+
+    def get_sections(
+        self, project_id: int, suite_id: Union[int, None] = None, model=Section
+    ):
+
         # suite id isn't required on projects that are operating as a "single suite"
         if suite_id:
             sections = self.send_get(f"/get_sections/{project_id}&suite_id={suite_id}")
         else:
             sections = self.send_get(f"/get_sections/{project_id}")
-        return [Section(**_) for _ in sections]
+        return [model.from_get(_) for _ in sections]
+
 
     def delete_suite(self, suite_id: int):
         return self.send_post(f"/delete_suite/{suite_id}", {})
 
-    def get_case_fields(self):
-        return self.send_get("/get_case_fields")
-
     def update_case(self, case: Case):
-        response = self.send_post(f"/update_case/{case.id}", asdict(case))
-        field_names = set(f.name for f in fields(Case))
-        if response:
-            return Case(**{k:v for k,v in response.items() if k in field_names})
+        return case.from_get(
+            self.send_post(f"/update_case/{case.id}", case.to_post(case))
+        )
--- src/extended/models.py
+++ src/extended/models.py
@@ -1,45 +1,58 @@
+from abc import abstractmethod
+from dataclasses import asdict, dataclass, field
+
 from dataclasses import dataclass, field
 from typing import Any, List, Union
 
 
+
+class TestRailBase:
+    @classmethod
+    def from_get(cls, raw):
+        obj = cls(**raw)
+        return obj
+
+    def to_post(self):
+        self.validate_request()
+        return asdict(self)
+
+    @abstractmethod
+    def validate_request(self):
+        pass
+
+
 @dataclass
-class Case:
-    #     # plan on extending to include the required and then allow subclassing or mixin for extending
-    #     # these: https://www.gurock.com/testrail/docs/api/reference/cases#addcase
+class Case(TestRailBase):
+    # plan on extending to include the required and then allow subclassing or mixin for extending
+    # these: https://www.gurock.com/testrail/docs/api/reference/cases#addcase
+    # The estimate_forecast is a string time, e.g. “30s” or “1m 45s”
+    # The template_id requires TestRail 5.2 or later
+    # The type_id is 7, which is other by default.
+    # created_by,updated_by is the ID of the user
+    # created_on,updated_on is a UNIX timestamp
+    # refs is a comma separated list of requirements, e.b. "JIRA-123,JIRA-124,JIRA-125"
     title: str
+
+    created_by: Union[None, int] = None
+    created_on: Union[None, str] = None
+    estimate: Union[None, str] = None
+    estimate_forecast: Union[None, str] = None
     id: Union[None, int] = None
-    type_id: int = 7  # 7 is Other by default, see get_case_types
+    template_id: Union[None, int] = None
+    type_id: int = 7
     priority_id: int = 1
     section_id: Union[None, int] = None
     suite_id: Union[None, int] = None
-    estimate: Union[None, str] = None
-    milestone_id: Union[
-        None, int
-    ] = None  # The ID of the milestone to link to the test case
-    refs: Union[None, str] = None  # A comma-separated list of references/requirements
-    template_id: Union[
-        None, int
-    ] = None  # The ID of the template (field layout) (requires TestRail 5.2 or later)
-    created_by: Union[None, int] = None  # The ID of the user who created the test case
-    created_on: Union[None, str] = None
-    estimate_forecast: Union[
-        None, str
-    ] = None  # The estimate forecast, e.g. “30s” or “1m 45s”
-    updated_by: Union[
-        None, int
-    ] = None  # The ID of the user who last updated the test case
-    updated_on: Union[
-        None, int
-    ] = None  # The date/time when the test case was last updated (as UNIX timestamp)
+    refs: Union[None, str] = None
+    updated_by: Union[None, int] = None
+    updated_on: Union[None, int] = None
+
     custom_steps_separated: Union[None, List] = None
     custom_steps: Union[None, List] = None
-    display_order: Union[None, List] = None
-    title: Union[None, str] = None
-    is_deleted: Union[None, bool] = None
-    custom_automation_type: Union[None, str] = None
+
 
 @dataclass
-class Section:
+class Section(TestRailBase):
     suite_id: int
     depth: int = 0
     parent_id: Union[None, int] = field(default=None)
@@ -50,14 +63,17 @@ class Section:
 
 
 @dataclass
-class Suite:
+class Suite(TestRailBase):
     name: str
     description: str
     project_id: int
-    url: str
-    is_completed: bool = None
-    id: int = None
-    is_baseline: bool = None
-    is_master: bool = None
-    completed_on: Any = None  # datetime?
-    # sections: List[Section] = field(repr=False, default_factory=list)
+    url: Union[str, None] = None
+    is_completed: Union[bool, None] = None
+    id: Union[int, None] = None
+    is_baseline: Union[bool, None] = None
+    is_master: Union[bool, None] = None
+    completed_on: Union[Any, None] = None
+
+    def to_post(self):
+        self.validate_request()
+        return {"name": self.name, "description": self.description}
