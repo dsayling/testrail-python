263ee03565d05a6578bcaa35f4cd1fe518f862f9
Drew Ayling <drew.ayling@livongo.com>
2021-09-21 15:22:13 -0600

    try tag with prefix

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
 from setuptools import setup
 
-setup(use_scm_version=True)
+setup(
+    use_scm_version=dict(tag_regex=r"^(?P<prefix>example-)?(?P<version>[vV]?\d+(?:\.\d+){0,2}[^\+]*)(?:\+.*)?$")
+    )
